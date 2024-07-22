12a4937014fe8b6b95f9433a7e85613ccb477160
MirceaA04 <mirceaa04@gmail.com>
2024-07-22 19:04:09 +0300

    first commit

---
+++ src/main/java/com/example/AplicatieMeditatii/AplicatieMeditatiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AplicatieMeditatii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicatieMeditatiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicatieMeditatiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AplicatieMeditatii/AplicatieMeditatiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AplicatieMeditatii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicatieMeditatiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
